339eecb0b4c217f25b0b5e222bd991b08d2b2c60
Luan <42867942+luansapelli@users.noreply.github.com>
2024-02-08 23:42:45 -0300

    add some persistence operations

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-use chrono;
 use sqlx::{PgPool};
+
+#[derive(sqlx::FromRow, sqlx::Decode)]
+struct Client {
+    limit_value: i32,
+    balance: i32,
+}
 
 #[derive(serde::Deserialize)]
 struct TransactionRequest {
     tipo: String,
-    valor: i64,
+    valor: i32,
     descricao: String,
 }
 
 #[derive(serde::Serialize)]
 struct TransactionResponse {
-    limite: i64,
-    saldo: i64,
+    limite: i32,
+    saldo: i32,
 }
 
 #[derive(serde::Serialize)]
@@ -23,31 +28,50 @@ pub struct AccountStatementResponse {
 
 #[derive(serde::Serialize)]
 pub struct Balance {
-    pub total: i64,
+    pub total: i32,
     pub data_extrato: String,
-    pub limite: i64,
+    pub limite: i32,
 }
 
 #[derive(serde::Serialize)]
 pub struct TransactionInfo {
-    pub valor: i64,
+    pub valor: i32,
     pub tipo: String,
     pub descricao: String,
     pub realizada_em: String,
 }
 
 async fn do_transaction(path: web::Path<(i16,)>, transaction: web::Json<TransactionRequest>, db_pool: web::Data<PgPool>) -> impl Responder {
-    if path.0 < 0 || path.0 > 5 {
-        return HttpResponse::NotFound().finish();
-    }
-
     if transaction.tipo != "c" && transaction.tipo != "d" {
         return HttpResponse::BadRequest().finish();
     }
 
-    if transaction.descricao.len() < 1 || transaction.descricao.len() > 10 {
+    if transaction.descricao.is_empty() || transaction.descricao.len() > 10 {
         return HttpResponse::BadRequest().finish();
     }
+
+    let client = match sqlx::query_as::<_, Client>(
+        "SELECT * FROM clients WHERE id = $1",
+    )   .bind(path.0)
+        .fetch_one(db_pool.get_ref())
+        .await
+    {
+        Ok(client) => client,
+        Err(_) => return HttpResponse::NotFound().finish(),
+    };
+
+    let new_balance = match transaction.tipo.as_str() {
+        "c" => client.balance + transaction.valor,
+        "d" => {
+            let potential_balance = client.balance - transaction.valor;
+            if potential_balance < -client.limit_value {
+                return HttpResponse::UnprocessableEntity().finish();
+            }
+
+            potential_balance
+        },
+        _ => return HttpResponse::BadRequest().finish(),
+    };
 
     let mut db_transaction = db_pool.begin().await.expect("Can not start transaction");
     match sqlx::query(r#"INSERT INTO transactions (client_id, value, "type", description) VALUES ($1, $2, $3, $4)"#)
@@ -59,6 +83,19 @@ async fn do_transaction(path: web::Path<(i16,)>, transaction: web::Json<Transact
         .await
     {
         Ok(_) => {
+            match sqlx::query(r#"UPDATE clients SET balance = $1 WHERE id = $2"#)
+                .bind(new_balance)
+                .bind(path.0)
+                .execute(&mut *db_transaction)
+                .await
+            {
+                Ok(_) => {},
+                Err(_) => {
+                    db_transaction.rollback().await.expect("Can not rollback transaction");
+                    return HttpResponse::InternalServerError().finish();
+                }
+            }
+
             db_transaction.commit().await.expect("Can not commit transaction");
         }
         Err(_) => {
@@ -67,9 +104,10 @@ async fn do_transaction(path: web::Path<(i16,)>, transaction: web::Json<Transact
         }
     }
 
+    //TODO -> remove mocked response
     HttpResponse::Ok().json(TransactionResponse {
-        limite: 1000,
-        saldo: 1000,
+        limite: client.limit_value,
+        saldo: new_balance
     })
 }
 
@@ -104,10 +142,9 @@ async fn fetch_account_statement(path: web::Path<(i16,)>) -> impl Responder {
 
 #[tokio::main]
 async fn main() {
-
     let db_pool = PgPool::connect("postgres://postgres:password@localhost/rinha").await.expect("Can not connect to database");
 
-    let _server = HttpServer::new(move || {
+    HttpServer::new(move || {
         App::new()
             .route("/clientes/{id}/transacoes", web::post().to(do_transaction))
             .route("/clientes/{id}/extrato", web::get().to(fetch_account_statement))
